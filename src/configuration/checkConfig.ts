986d0f47afc8d02e8fdf8406630e051750c986a8
Eryk Szpotanski <eszpotanski@antmicro.com>
2025-05-21 13:56:06 +0200

    configuration: checkConfig: Set process environment based on the plugin config and improve validation
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

--- src/configuration/checkConfig.ts
+++ src/configuration/checkConfig.ts
@@ -6,16 +6,39 @@
  */
 
 
-import { existsSync } from 'fs';
+import { existsSync, lstatSync } from 'fs';
 import path from 'path';
 import * as vscode from 'vscode';
 import { KChannel } from '../utils';
+import { RunAutoMLState } from './messageTypes';
+
+/**
+ * Sets up environmental variables based on the plugin config
+ */
+function prepareEnv() {
+    const conf2Var = {
+        "zephyrSDKPath": "ZEPHYR_SDK_PATH",
+        "pyrenodePath": "PYRENODE_PKG",
+        "ai8x-training": "AI8X_TRAINING_PATH",
+        "ai8x-synthesis": "AI8X_SYNTHESIS_PATH",
+    };
+
+    const pluginConfig = vscode.workspace.getConfiguration("kenning-edge-automl");
+
+    Object.entries(conf2Var).forEach((conf) => {
+        if (pluginConfig.get(conf[0])) {
+            process.env[conf[1]] = pluginConfig.get(conf[0]);
+        }
+    });
+}
 
 /**
  * Checks whether the configuration is correct and required paths exist
  * @returns True if configuration is correct
  */
-export function checkConfig(): boolean {
+export function checkConfig(autoMLConf: RunAutoMLState): boolean {
+    prepareEnv();
+
     const pluginConfig = vscode.workspace.getConfiguration("kenning-edge-automl");
 
     const errors: string[] = [];
@@ -41,28 +64,31 @@ export function checkConfig(): boolean {
         }
     }
 
-    const renodeRuntime = process.env.PYRENODE_RUNTIME ?? 'mono';
+    // Validate PYRENODE only if simulation is enabled
+    if (autoMLConf.simulate) {
+        const renodeRuntime = process.env.PYRENODE_RUNTIME ?? 'mono';
 
-    if (renodeRuntime === 'mono') {
-        const renodePortablePath = process.env.PYRENODE_PKG;
+        if (renodeRuntime === 'mono') {
+            const renodePortablePath = process.env.PYRENODE_PKG;
 
-        // Check if path to Renode portable is set and exists
-        if (renodePortablePath === undefined) {
-            errors.push(`PYRENODE_PKG is not set`);
-        } else if (!existsSync(renodePortablePath)) {
-            errors.push(`Renode portable not found at ${renodePortablePath}`);
+            // Check if path to Renode portable is set and exists
+            if (renodePortablePath === undefined) {
+                errors.push(`PYRENODE_PKG is not set`);
+            } else if (!existsSync(renodePortablePath)) {
+                errors.push(`Renode portable not found at ${renodePortablePath}`);
+            }
+        } else if (renodeRuntime === 'coreclr') {
+            const renodeBinPath = process.env.PYRENODE_BIN;
+
+            // Check if path to Renode executable is set and exists
+            if (renodeBinPath === undefined) {
+                errors.push(`PYRENODE_BIN is not set`);
+            } else if (!existsSync(renodeBinPath)) {
+                errors.push(`Renode executable not found at ${renodeBinPath}`);
+            }
+        } else {
+            errors.push(`Invalid RENODE_RUNTIME ${renodeRuntime}`);
         }
-    } else if (renodeRuntime === 'coreclr') {
-        const renodeBinPath = process.env.PYRENODE_BIN;
-
-        // Check if path to Renode executable is set and exists
-        if (renodeBinPath === undefined) {
-            errors.push(`PYRENODE_BIN is not set`);
-        } else if (!existsSync(renodeBinPath)) {
-            errors.push(`Renode executable not found at ${renodeBinPath}`);
-        }
-    } else {
-        errors.push(`Invalid RENODE_RUNTIME ${renodeRuntime}`);
     }
 
     const zephyrSdkPath = process.env.ZEPHYR_SDK_PATH;
@@ -72,6 +98,36 @@ export function checkConfig(): boolean {
         errors.push(`ZEPHYR_SDK_PATH is not set`);
     } else if (!existsSync(zephyrSdkPath)) {
         errors.push(`Zephyr SDK not found at ${zephyrSdkPath}`);
+    }
+
+    // Check ai8x repositories
+    if (autoMLConf?.optimizer === "Ai8xCompiler") {
+        const ai8xTrainingPath = process.env.AI8X_TRAINING_PATH;
+        if (ai8xTrainingPath === undefined) {
+            errors.push(`AI8X_TRAINING_PATH is not defined`);
+        } else if (!existsSync(ai8xTrainingPath)) {
+            errors.push(`AI8X_TRAINING_PATH not found at ${ai8xTrainingPath}`);
+        } else if(!existsSync(path.join(ai8xTrainingPath, ".venv"))) {
+            errors.push(`AI8X_TRAINING_PATH not prepared, missing virtual env at ${ai8xTrainingPath}/.venv`);
+        }
+
+        const ai8xSynthesisPath = process.env.AI8X_SYNTHESIS_PATH;
+        if (ai8xSynthesisPath === undefined) {
+            errors.push(`AI8X_SYNTHESIS_PATH is not defined`);
+        } else if (!existsSync(ai8xSynthesisPath)) {
+            errors.push(`AI8X_SYNTHESIS_PATH not found at ${ai8xSynthesisPath}`);
+        } else if(!existsSync(path.join(ai8xSynthesisPath, ".venv"))) {
+            errors.push(`AI8X_SYNTHESIS_PATH not prepared, missing virtual env at ${ai8xSynthesisPath}/.venv`);
+        }
+    }
+
+    const openocdPath = pluginConfig.get<string>("openocdPath");
+    if (openocdPath) {
+        if (!existsSync(openocdPath)) {
+            errors.push(`OpenOCD not found at ${openocdPath}`);
+        } else if (!lstatSync(openocdPath).isFile()) {
+            errors.push(`OpenOCD is not a file ${openocdPath}`);
+        }
     }
 
     if (errors.length > 0) {
